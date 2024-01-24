92a8181cea760663f5b34a8e521f32c8c4c6a9d6
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-23 23:48:59 -0600

    new file:   Codecademy-C-.sln new file:   Conce/DataTypesVariables/Drill/dataTypes/Program.cs new file:   Conce/DataTypesVariables/Drill/dataTypes/bin/Debug/net8.0/dataTypes.deps.json new file:   Conce/DataTypesVariables/Drill/dataTypes/bin/Debug/net8.0/dataTypes.dll new file:   Conce/DataTypesVariables/Drill/dataTypes/bin/Debug/net8.0/dataTypes.exe new file:   Conce/DataTypesVariables/Drill/dataTypes/bin/Debug/net8.0/dataTypes.pdb new file:   Conce/DataTypesVariables/Drill/dataTypes/bin/Debug/net8.0/dataTypes.runtimeconfig.json new file:   Conce/DataTypesVariables/Drill/dataTypes/dataTypes.csproj new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/apphost.exe new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.AssemblyInfo.cs new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.AssemblyInfoInputs.cache new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.GeneratedMSBuildEditorConfig.editorconfig new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.GlobalUsings.g.cs new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.assets.cache new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.csproj.CoreCompileInputs.cache new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.csproj.FileListAbsolute.txt new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.dll new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.genruntimeconfig.cache new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.pdb new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.sourcelink.json new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/ref/dataTypes.dll new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/refint/dataTypes.dll new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/dataTypes.csproj.nuget.dgspec.json new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/dataTypes.csproj.nuget.g.props new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/dataTypes.csproj.nuget.g.targets new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/project.assets.json new file:   Conce/DataTypesVariables/Drill/dataTypes/obj/project.nuget.cache new file:   QA/.NET.md

---
+++ Conce/DataTypesVariables/Drill/dataTypes/Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+1.
+
+To practice creating variables, we’re going to write a program that prints information about a dog to the console. We’ll be working with the types string, int, double, and bool.
+
+First, create two string variables. The first one is called name and has the value "Shadow".
+The second one is called breed and has the value "Golden Retriever".
+Checkpoint 2 Passed
+
+2.
+
+Next, create a variable to hold the age. Name the variable age and store the value 5.
+Checkpoint 3 Passed
+
+3.
+
+Next, create a variable to hold the weight. Name the variable weight and store the value 65.22.
+Checkpoint 4 Passed
+
+4.
+
+Next, create a variable that can be either true or false. Name the variable spayed and set it to true.
+Checkpoint 5 Passed
+
+5.
+
+Use Console.WriteLine() to print each variable to the console.
+
+*/
+
+namespace CSharpFundamentals
+{
+   class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Data Types and Variables!\n");
+      // Create Variables
+        string name = "Shadow";
+        string breed = "Golden Retriever";
+        int age = 5;
+        double weight = 65.22;
+        bool spayed = true;
+
+            // Print variables to the console
+        Console.WriteLine(name);
+        Console.WriteLine(breed);
+        Console.WriteLine(age);
+        Console.WriteLine(weight);
+        Console.WriteLine(spayed);
+
+
+    }
+  }
+}
