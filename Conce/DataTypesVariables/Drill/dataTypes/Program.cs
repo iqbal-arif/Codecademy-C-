26a0f55df86848d44b5bcfb483bc4c69d295c226
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-26 23:35:07 -0600

    modified:   Conce/DataTypesVariables/Drill/dataTypes/Program.cs

--- Conce/DataTypesVariables/Drill/dataTypes/Program.cs
+++ Conce/DataTypesVariables/Drill/dataTypes/Program.cs
@@ -94,8 +94,20 @@ namespace CSharpFundamentals
         Console.Write("Enter your favorite number!: ");
         int faveNumber = Convert.ToInt32(Console.ReadLine());
 
+        //Char and String DataType
+        string drink = "wine";
+      string madTeaParty = $"\"Have some {drink},\" the March Hare said in an encouraging tone. \nAlice looked all round the table, but there was nothing on it but tea.\n\"I don't see any {drink},\" she remarked.\n\"There isn't any,\" said the March Hare.";
 
-        
+      int storyLength = madTeaParty.Length;
+      string toFind = "March Hare";
+
+      string findLowerCase = toFind.ToLower(); 
+      int findMarchHare = madTeaParty.IndexOf(toFind);
+
+      Console.WriteLine(madTeaParty.Substring(findMarchHare));
+      Console.WriteLine($"This scene is {storyLength} long.\n");
+      Console.WriteLine($"The term we're looking for is {toFind} and is located at index {findMarchHare}.");
+
     }
 
     
