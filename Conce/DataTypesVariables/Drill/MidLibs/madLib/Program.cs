205c687ab7707f6bd7dfc9ef607d0978d2f5ca5b
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-28 22:54:40 -0600

    modified:   Conce/DataTypesVariables/Drill/MidLibs/madLib/Program.cs modified:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.AssemblyInfo.cs modified:   Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.AssemblyInfoInputs.cache

--- Conce/DataTypesVariables/Drill/MidLibs/madLib/Program.cs
+++ Conce/DataTypesVariables/Drill/MidLibs/madLib/Program.cs
@@ -12,15 +12,33 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Mad Libs has started");
 
       // Give the Mad Lib a title:
-      string title = "Chilling Story";
+      string title = "Guess The Story";
 
       Console.WriteLine(title);
       // Define user input and variables:
 
-//string userInput = Console.ReadLine();
 Console.Write("Enter a name:");
 string name = Console.ReadLine();
+Console.Write("Enter first Adjective:");
+string firstAdj = Console.ReadLine();
+Console.Write("Enter Second Adjective:");
+string secondtAdj = Console.ReadLine();
+Console.Write("Enter Third Adjective:");
+string thirdAdj = Console.ReadLine();
+Console.Write("Enter The Verb:");
+string verb = Console.ReadLine();
+Console.Write("Enter The Frist Noun:");
+string firstNoun = Console.ReadLine();
+Console.Write("Enter The Second Noun:");
+string secondNoun = Console.ReadLine();
 
+var animal = "An Animal";
+var food = "A Food";
+var fruit = "A Fruit";
+var superHero = "A SuperHero";
+var country = "A Country";
+var dessert = "A Dessert";
+var year = "A Year";
 
       // The template for the story:
       string story = "This morning _ woke up feeling _. 'It is going to be a _ day!' Outside, a bunch of _s were protesting to keep _ in stores. They began to _ to the rhythm of the _, which made all the _s very _. Concerned, _ texted _, who flew _ to _ and dropped _ in a puddle of frozen _. _ woke up in the year _, in a world where _s ruled the world.";
--- Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.AssemblyInfo.cs
+++ Conce/DataTypesVariables/Drill/dataTypes/obj/Debug/net8.0/dataTypes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dataTypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68f9d218494d6d633715574d8406edb88456491d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+103dcb02b518e751adefa5cd5931c3b8d3221718")]
 [assembly: System.Reflection.AssemblyProductAttribute("dataTypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dataTypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
