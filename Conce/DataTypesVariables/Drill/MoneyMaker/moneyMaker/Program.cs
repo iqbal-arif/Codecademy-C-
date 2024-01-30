145df3fb591c3d2a1f9ecafca10d771d4f0342bd
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-29 23:51:59 -0600

    modified:   Conce/DataTypesVariables/Drill/MoneyMaker/moneyMaker/Program.cs

--- Conce/DataTypesVariables/Drill/MoneyMaker/moneyMaker/Program.cs
+++ Conce/DataTypesVariables/Drill/MoneyMaker/moneyMaker/Program.cs
@@ -7,7 +7,12 @@ namespace MoneyMaker
     public static void Main(string[] args)
     {
       Console.WriteLine("Welcome to Money Maker!");
+      Console.WriteLine("Enter an amount to convert to coins:");
+      var userInput = Convert.ToDouble(Console.ReadLine());
+
+
       
+      Console.WriteLine($"{userInput} is equal to !");
     }
   }
 }
