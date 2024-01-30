93e14bf99ff101c41085f0000abf61faf0b4a9ca
Arif Iqbal <arif_iqbal@yahoo.com>
2024-01-29 23:42:52 -0600

    modified:   Conce/DataTypesVariables/Drill/MoneyMaker/moneyMaker/Program.cs new file:   Conce/DataTypesVariables/Drill/MoneyMaker/moneyMaker/README.md

--- Conce/DataTypesVariables/Drill/MoneyMaker/moneyMaker/Program.cs
+++ Conce/DataTypesVariables/Drill/MoneyMaker/moneyMaker/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MoneyMaker
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to Money Maker!");
+      
+    }
+  }
+}
