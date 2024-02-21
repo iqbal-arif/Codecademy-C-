d4f527979c67dc4f01e91e9b6c90f4515e32cce6
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-20 22:11:21 -0600

    Implemented Encrypt Method

--- Project/CaesarCipher/CaesarCipher/Program.cs
+++ Project/CaesarCipher/CaesarCipher/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System.Net.Security;
+
+class Program
 {
     public static void Main(string[] args)
     {
@@ -6,26 +8,63 @@
 
         char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
+        //char[] symbol = new char[] { '~', "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", ":", ";", "<", "<", "?", ",", "|", "\\", "/", "-" };
         Console.WriteLine("Write you Secret message here");
 
+
+
+        //String UpperCase to LowerCase
         string secretInput = Console.ReadLine().ToLower();
 
+        //Removine empyt space with Replace method
         secretInput = secretInput.Replace(" ", "");
+
 
         // Console.WriteLine(secretInput);
 
+        //Separating each word to a char-array
         char[] secretMessage = secretInput.ToCharArray();
 
 
-        char[] encryptedMessage = new char [secretMessage.Length];
-            string finalMessage = "";
+        
+
+
+        Encrypt(secretMessage);
+
+
+        
+
+    }
+
+    static void Encrypt(char[] userString)
+    {
+
+        //Declaring encryptedMessage Array
+        char[] encryptedMessage = new char[userString.Length];
+
 
         for (int i = 0; i < secretMessage.Length; i++)
         {
             int shiftIndex = 0;
-            char messageAlphabet = secretMessage[i];
+
+            //Replacing Special Character with "x"
+            if (Char.IsLetter(secretMessage[i]))
+            {
+                secretMessage[i] = secretMessage[i];
+            }
+            else
+            {
+                secretMessage[i] = 'x';
+            }
+
+
+            //Finding the char value at index and displaying it :TP
+            /*char messageAlphabet = secretMessage[i];
+            Console.WriteLine(messageAlphabet);*/
+
+            //Getting the index of message letter through alphabet char-array
             int index = Array.IndexOf(alphabet, secretMessage[i]);
-            
+            /*
             if (index > 22)
             {
                 shiftIndex = index - 23;
@@ -36,9 +75,19 @@
                 shiftIndex = index + 3;
                 encryptedMessage[i] = alphabet[shiftIndex];
             }
+            */
+
+            shiftIndex = (index + 3) % alphabet.Length;
+            encryptedMessage[i] = alphabet[shiftIndex];
         }
-            Console.WriteLine(encryptedMessage);
-            Console.ReadLine();
-          
+        Console.WriteLine(encryptedMessage);
+        Console.ReadLine();
+
+        /*for (int i = 0; i < userString.Length; i++)
+        {
+            Console.WriteLine("Method Encrypt: " + userString[i]);
+        }*/
+
+
     }
 }
