c895505e33a057d421897810fcc815bde9b8673a
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-22 22:12:08 -0600

    Adding scroing capibility

--- Project/TrueOrFalse/TrueOrFalse/TrueOrFalse/Program.cs
+++ Project/TrueOrFalse/TrueOrFalse/TrueOrFalse/Program.cs
@@ -8,8 +8,8 @@
 
             // Do not edit these lines
             Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
-            string entry = Console.ReadLine();
-            Tools.SetUpInputStream(entry);
+            //string entry = Console.ReadLine();
+            //Tools.SetUpInputStream(entry);
 
             // Type your code below
 
@@ -25,9 +25,11 @@
             }
 
 
+                int aksingIndex = 0;
+                int scoringIndex = 0;
+                 int score = 0;
             foreach (var question in questions)
             {
-                int aksingIndex = 0;
                 string input = "";
                 bool isBool ;
                 bool inputBool ;
@@ -35,26 +37,53 @@
                 Console.WriteLine("True or False");
                 input = Console.ReadLine();
 
-                isBool = Boolean.TryParse(input , out inputBool);
+                isBool = Boolean.TryParse(input , out inputBool); //True or False
+               // int.TryParse(input , out inputInt); //True or False
 
                 
 
 
-                while (isBool) //false to stop
+                while (!isBool) //false to stop
                 {
                     Console.WriteLine("Please respond with 'true' or 'false'.");
                     input = Console.ReadLine(); 
-                    Boolean.TryParse(input , out isBool);
+                    isBool = Boolean.TryParse(input , out inputBool);
                 }
                    Console.WriteLine("Inside While: " + isBool);
-                responses[aksingIndex] = isBool;
-                aksingIndex ++;
+                    responses[aksingIndex++] = inputBool;
+                Console.WriteLine("Answer Value from Answer Array " + answers[ scoringIndex]);
+
+                if (answers[scoringIndex] == responses[scoringIndex])
+                {
+                    score++;
+                }
+                scoringIndex++;
+                
             }
+            Console.WriteLine(score);
+
+
             foreach (var response in responses)
             {
 
-            Console.WriteLine(response);
+            Console.WriteLine("Display response"+response);
+
             }
+
+
+            foreach (var item in answers)
+            {
+                if (responses[aksingIndex] == answers[aksingIndex])
+                {
+                    score++;
+                }
+                Console.WriteLine(item);
+            }
+            
+                
+         
+
+
         }
     }
 }
