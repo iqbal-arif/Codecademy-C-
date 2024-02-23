c5e89ab12673f83d72e92c062f2d52ab83d5d63f
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-22 22:41:59 -0600

    App is completed

--- Project/TrueOrFalse/TrueOrFalse/TrueOrFalse/Program.cs
+++ Project/TrueOrFalse/TrueOrFalse/TrueOrFalse/Program.cs
@@ -4,84 +4,63 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("True Or False!\n");
-
             // Do not edit these lines
             Console.WriteLine("Welcome to 'True or False?'\nPress Enter to begin:");
-            //string entry = Console.ReadLine();
-            //Tools.SetUpInputStream(entry);
+            string entry = Console.ReadLine();
+            Tools.SetUpInputStream(entry);
 
             // Type your code below
+            var questions = new string[] { "Eggplants are type of berry?", "Eggplants are a species in the nightshade family." };
 
-            var questions = new string[] {"Eggplants are type of berry?", "Eggplants are a species in the nightshade family." };
+            var answers = new bool[] { true, false };
 
-            var answers = new bool[] {true,false };
+            var responses = new bool[answers.Length];
 
-            var responses = new bool[answers.Length];   
-
-            if (questions.Length != answers.Length) 
+            if (questions.Length != answers.Length)
             {
                 Console.WriteLine("Warning! ");
             }
 
-
-                int aksingIndex = 0;
-                int scoringIndex = 0;
-                 int score = 0;
+            int askingIndex = 0;
+            int scoringIndex = 0;
+            int score = 0;
             foreach (var question in questions)
             {
                 string input = "";
-                bool isBool ;
-                bool inputBool ;
+                bool isBool;
+                bool inputBool;
                 Console.WriteLine(question);
                 Console.WriteLine("True or False");
                 input = Console.ReadLine();
 
-                isBool = Boolean.TryParse(input , out inputBool); //True or False
-               // int.TryParse(input , out inputInt); //True or False
+                isBool = Boolean.TryParse(input, out inputBool); //True or False
+                // int.TryParse(input , out inputInt); //True or False
 
-                
-
-
-                while (!isBool) //false to stop
-                {
+                while (!isBool)
+                { //false to stop
                     Console.WriteLine("Please respond with 'true' or 'false'.");
-                    input = Console.ReadLine(); 
-                    isBool = Boolean.TryParse(input , out inputBool);
+                    input = Console.ReadLine();
+                    isBool = Boolean.TryParse(input, out inputBool);
                 }
-                   Console.WriteLine("Inside While: " + isBool);
-                    responses[aksingIndex++] = inputBool;
-                Console.WriteLine("Answer Value from Answer Array " + answers[ scoringIndex]);
+               
+                responses[askingIndex] = inputBool;
 
                 if (answers[scoringIndex] == responses[scoringIndex])
                 {
                     score++;
                 }
+                Console.WriteLine("Input : " + responses[askingIndex] + "| Answer : " + answers[scoringIndex]);
+                askingIndex++;
                 scoringIndex++;
-                
-            }
-            Console.WriteLine(score);
-
-
-            foreach (var response in responses)
-            {
-
-            Console.WriteLine("Display response"+response);
-
             }
 
+            Console.WriteLine($"You got {score} out of {answers.Length} correct!");
 
-            foreach (var item in answers)
-            {
-                if (responses[aksingIndex] == answers[aksingIndex])
-                {
-                    score++;
-                }
-                Console.WriteLine(item);
-            }
-            
-                
-         
+
+
+
+
+
 
 
         }
