434197326afaa11a9ca5b78351afe86af8b5297c
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-24 22:59:16 -0600

    Updated MatchProfile app

---
+++ Project/MatchProfile/MatchProfile/MatchProfile/Profile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchProfile
+{
+    public class Profile
+    {
+
+        // FIELDS
+
+        private string name;
+        private int age;
+        private string city;
+        private string country;
+        private string pronouns;
+        private string[] hobbies;
+
+        //Properties are used to:
+        //validate values
+        //control external access
+
+        //CONSTRUCTOR
+
+        public Profile(string name, int age, string city, string country, string pronouns = "He/She")
+        {
+            this.name = name;
+            age = age;
+            this.city = city;
+            this.country = country;
+            this.hobbies =  hobbies;
+
+        }
+
+        //METHOD
+
+        public string ViewProfile()
+        {
+            foreach(var hobb in hobbies)
+            {
+                return hobb;
+            }
+
+            string userProfile = $"Name: {name};\n Age: {age};\n City: {city};\n Country: {country};\n Hobbies :{hobbies}";
+
+            return userProfile;
+        }
+
+        
+        public void SetHobbies(string[] hobbies)
+        {
+            this.hobbies = hobbies;
+        }
+    }
+}
--- Project/MatchProfile/MatchProfile/MatchProfile/Program.cs
+++ Project/MatchProfile/MatchProfile/MatchProfile/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace MatchProfile
+﻿using MatchProfile;
+
+namespace MatchProfile
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Matching Profile!");
+
+            Profile sam = new Profile("Sam Drakkila", 30, "New York", "USA");
+
+            sam.SetHobbies(new string[] { "listening to audiobooks and podcsts", "playing rec sports like bowling and kickball", "writing a specualtive fiction novel", "rading advice columns" });
+
+            Console.WriteLine(sam.ViewProfile);
+
+
+            Console.ReadLine();
         }
     }
 }
