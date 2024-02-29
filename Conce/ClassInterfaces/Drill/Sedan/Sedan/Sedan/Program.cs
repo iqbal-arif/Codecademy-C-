6abff094da29067977578a39c6d146dfee942a45
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-28 23:51:43 -0600

    Tool.cs added

--- Conce/ClassInterfaces/Drill/Sedan/Sedan/Sedan/Program.cs
+++ Conce/ClassInterfaces/Drill/Sedan/Sedan/Sedan/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Sedan!\n");
+
+            Sedan s = new Sedan(60);
+            Console.WriteLine($"Sedan with license plate {s.LicensePlate} and {s.Wheels} wheels, driving at {s.Speed} km/h.");
+            s.Honk();
         }
     }
 }
