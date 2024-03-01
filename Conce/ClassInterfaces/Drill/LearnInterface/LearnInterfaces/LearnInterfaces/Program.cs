75685bdac62e1193f1e54122f5bc2e4d4cc1cee9
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-29 23:52:17 -0600

    Added LearnInterface.cs Initial Commit

---
+++ Conce/ClassInterfaces/Drill/LearnInterface/LearnInterfaces/LearnInterfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LearnInterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LearnInterfaces!\n");
+
+
+        }
+    }
+}
