9c71363abc588831b159521fa2988f47405d787b
Arif Iqbal <arif_iqbal@yahoo.com>
2024-03-07 00:05:40 -0600

    Lists Initial Commit

---
+++ Conce/Lists/Concept/Lists/Lists/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn Lists!\n");
+
+            List<string> citiesList = new List<string> { "Delhi", "Los Angeles", "Saint Petersburg" };
+
+            citiesList.Add("New York City");
+
+            citiesList.Remove("Delhi");
+
+            citiesList.AddRange(new string[] { "Cairo", "Johannesburg" });
+
+            bool hasNewDelhi = citiesList.Contains("New Delhi");
+
+            foreach (string city in citiesList)
+            {
+                Console.WriteLine(city);
+            }
+        }
+    }
+}
