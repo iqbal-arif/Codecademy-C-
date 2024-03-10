441459eaed5b9e06484fb6fcd39e79e13c39dd7f
Arif Iqbal <arif_iqbal@yahoo.com>
2024-03-10 00:42:59 -0600

    LINQ drill updated

--- Project/ProgrammingLanguages/ProgrammingLanguages/ProgrammingLanguages/Program.cs
+++ Project/ProgrammingLanguages/ProgrammingLanguages/ProgrammingLanguages/Program.cs
@@ -26,6 +26,60 @@
                 Console.WriteLine($"{item.Year}  {item.Name}  {item.ChiefDeveloper}  {item.Predecessors}");
             }
 
+            //C# RELEASED ON
+
+            var cSharp = languages.Where(x => x.Name =="C#").Select(x => x.Prettify()).ToList();
+
+            foreach (var cs in cSharp)
+            {
+                Console.WriteLine(cs);
+            }
+
+            //MICROSOFT DEVELOPED LANGUAGES
+
+           // var msLanguages = languages.Where(x => x.ChiefDeveloper == "Microsoft").Select(x => x.Prettify()).ToList();
+
+            var msLanguages = languages.Where(name => name.ChiefDeveloper.Contains("Microsoft")).Select(x => x.Prettify()).ToList();
+            foreach(var ms in msLanguages)
+            {
+                Console.WriteLine(ms);
+            }
+
+            // OTHER LAGUAGES BASED ON THIS LANGUAGE
+
+            //var lispLanguages = languages.Where(x => x.Predecessors == "Lisp").Select(x => x.Prettify()).ToList();
+            var lispLanguages = languages.Where(name => name.Name.Contains("Lisp")).Select(x => x.Prettify()).ToList();
+
+            foreach( var lisp in lispLanguages)
+            {
+                Console.WriteLine(lisp);
+            }
+
+            //LANGUAGES WITH "S" SCRIPT NAME
+
+            var scriptLanguages = languages.Where(name => name.Name.Contains("Script")).Select(name => name.Name);
+
+            foreach (var script in scriptLanguages)
+            {
+                Console.WriteLine(script);
+            }
+
+
+            //TOTAL NUMBER OF LANGUAGES
+
+            var totalLanguages = languages.Count;
+
+            Console.WriteLine(totalLanguages);
+
+            //TOTAL NUMBER OF LANGUAGES BETWEEN 1995-2005
+
+            var from1995To2005Languages = languages.Where(year => year.Year >= 1995 && year.Year <= 2005).Count();
+
+            Console.WriteLine(from1995To2005Languages);
+
+
+
+
 
 
             Console.ReadLine();
