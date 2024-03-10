fe356955b6bab6b8df73db8af4a20137a4ca6702
Arif Iqbal <arif_iqbal@yahoo.com>
2024-03-09 23:38:30 -0600

    Programming Language initail commit

---
+++ Project/ProgrammingLanguages/ProgrammingLanguages/ProgrammingLanguages/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ProgrammingLanguages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programming SLanguages!\n");
+
+            List<Language> languages = File.ReadAllLines("./languages.tsv")
+                .Skip(1)
+                .Select(line => Language.FromTsv(line))
+                .ToList();
+
+            //PRINT ALL LANGUAGES
+            /*
+                        foreach (Language language in languages)
+                        {
+                            Console.WriteLine(language.Prettify());
+
+                        }
+           */
+            //public Language(int year, string name, string chiefDeveloper, string predecessors)
+            //
+            /*
+            var languageYear = languages.Select(year => year);
+            var languageName = languages.Select(name => name);
+            var languageChiefDeveloper = languages.Select(chiefDeveloper => chiefDeveloper);
+            var languagePredecessors = languages.Select(predecessors => predecessors);
+            */
+            foreach (var item in languages)
+            {
+                Console.WriteLine($"{item.Year}  {item.Name}  {item.ChiefDeveloper}  {item.Predecessors}");
+            }
+
+
+            /*
+            var strLanguage = from year in languages
+                              from name in languages
+                              from chiefDeveloper in languages
+                              from predecessors in languages
+                              select (year,name,chiefDeveloper,predecessors);
+           
+           
+            foreach (var language in strLanguage)
+            {
+                Console.WriteLine($"{language.year.Year} ");
+                   // $"{ language.name.Name} {language.chiefDeveloper.ChiefDeveloper} {language.predecessors.Predecessors}");
+            }
+
+           */
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Project/ProgrammingLanguages/ReadMe.md
@@ -0,0 +1,24 @@
+**Programming Languages**
+-------------------------
+
+Do you know when C# was invented? How about your other favorite programming languages, like JavaScript, Ruby, and R?
+
+In this project you’ll be using lists and LINQ to search a database for answers!
+
+The data is stored in a .tsv file, which stands for tab-separated values. It’s a common way to export data from a spreadsheet or database — you might see this file type when you try to download data from apps like Microsoft Excel and Google Sheets.
+
+Here is what the data would look like as a table (just a few random rows included):
+
+Year 	Name 	            Chief Developer 	                        Predecessors
+1990 	AMOS BASIC 	        François Lionet, Constantin Sotiropoulos 	STOS BASIC
+1991 	Visual Basic 	    Alan Cooper (sold to Microsoft) 	        QuickBASIC
+1995 	Ruby 	            Yukihiro Matsumoto 	                        Smalltalk;Perl
+2009 	Go 	                Google 	                                    C;Oberon;Limbo;Smalltalk
+
+In Program.cs, the data importing is already taken care of, so you can focus on the queries! The data is stored in languages which is a list of type List<Language>. You can find the definition for the Language class in Language.cs. Here are the class’s important members:
+
+  1.  int Year — When the language was invented
+  2.  string Name — The name of the language
+  3.  string ChiefDeveloper — The head developer and/or company responsible for making the language
+  4.  string Predecessors — The other programming languages that this language is based on
+  5.  string Prettify() — Returns a nicely formatted string version of the object
